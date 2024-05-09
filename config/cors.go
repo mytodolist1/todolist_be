9b173978ebf43531d3294aa76780c97f096a3ead
dirgafebrian <dirgafebrian45@gmail.com>
2024-05-09 21:12:01 +0700

    Update CORS middleware to allow additional authorization header

--- config/cors.go
+++ config/cors.go
@@ -7,7 +7,7 @@ func CorsMiddleware(next http.Handler) http.Handler {
 		func(w http.ResponseWriter, r *http.Request) {
 			w.Header().Set("Access-Control-Allow-Origin", "*")
 			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
-			w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+			w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, AuthorizationA")
 			w.Header().Set("Access-Control-Max-Age", "3600")
 
 			if r.Method == http.MethodOptions {
