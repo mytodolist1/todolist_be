cd9caa30952a993344d761592e2197cced29ff80
dirgafebrian <dirgafebrian45@gmail.com>
2024-05-10 08:33:40 +0700

    Refactor CORS middleware to allow specific origins and methods for API endpoints

--- config/cors.go
+++ config/cors.go
@@ -1,16 +1,16 @@
 package config
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
-// CorsMiddleware dengan metode dan origin yang diizinkan.
 func CorsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(
 		func(w http.ResponseWriter, r *http.Request) {
-			// Tetapkan origin yang diizinkan
 			allowedOrigins := map[string]bool{
-				"https://mytodolist.my.id": true,
-				"http://127.0.0.1:5500":    true,
 				"http://127.0.0.1:5501":    true,
+				"https://mytodolist.my.id": true,
 			}
 
 			origin := r.Header.Get("Origin")
@@ -19,19 +19,27 @@ func CorsMiddleware(next http.Handler) http.Handler {
 				w.Header().Set("Access-Control-Allow-Origin", origin)
 				w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 				w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+				w.Header().Set("Access-Control-Allow-Credentials", "true")
 				w.Header().Set("Access-Control-Max-Age", "3600")
 
-				// Jika preflight
 				if r.Method == http.MethodOptions {
 					w.WriteHeader(http.StatusNoContent)
 					return
 				}
 			} else {
-				w.WriteHeader(http.StatusForbidden) // Origin tidak diizinkan
+				w.WriteHeader(http.StatusForbidden)
 				return
 			}
 
 			next.ServeHTTP(w, r)
 		},
 	)
+}
+
+func LoggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+			fmt.Println("Received request:", r.Method, r.URL.Path)
+			next.ServeHTTP(w, r)
+		})
 }
